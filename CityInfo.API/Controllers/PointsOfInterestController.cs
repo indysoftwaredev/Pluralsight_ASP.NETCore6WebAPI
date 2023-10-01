8657dbbc0f3fbdf8513c8ae5a732beb7ffeae18e
David Carter <indysoftwaredev@gmail.com>
2023-10-01 09:21:11 -0400

    implement DELETE - note: I'm not fond of the repeated duplication in the demo code, but keeping it here to follow along with the rest of the training.  Further modules include Services, DI, and Entity Framework

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -137,5 +137,25 @@ namespace CityInfo.API.Controllers
             return NoContent();
 
         }
+
+        [HttpDelete("{pointofinterestid}")]
+        public ActionResult DeletePointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterestFromStore = city.PointsOfInterest
+                .FirstOrDefault(p => p.Id == pointOfInterestId);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+            return NoContent();
+
+        }
     }
 }
