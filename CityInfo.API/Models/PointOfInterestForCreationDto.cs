d7fac3d5ac99c598584ee20624e88e6163261def
David Carter <indysoftwaredev@gmail.com>
2023-10-01 08:37:48 -0400

    adding some basic validation on point of interest creation

--- CityInfo.API/Models/PointOfInterestForCreationDto.cs
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -1,8 +1,14 @@
-﻿namespace CityInfo.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
 {
     public class PointOfInterestForCreationDto
     {
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
         public string? Description { get; set; }
     }
 }
