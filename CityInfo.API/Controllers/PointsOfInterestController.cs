8827a267f9831433d93d14efd4c2397feef3dffb
David Carter <indysoftwaredev@gmail.com>
2023-10-01 08:47:59 -0400

    added PUT update functionality for a point of interest

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -71,5 +71,28 @@ namespace CityInfo.API.Controllers
                 finalPointOfInterest);
             
         }
+
+        [HttpPut("{pointOfInterestId}")]
+        public ActionResult UpdatePointOfInterest(int cityId, int pointOfInterestId, 
+            PointOfInterestForUpdateDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            var pointOfInterestFromStore = city.PointsOfInterest
+                .FirstOrDefault(p => p.Id == pointOfInterestId);
+            if(pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore .Description = pointOfInterest.Description;
+
+            return NoContent();
+
+        }
     }
 }
