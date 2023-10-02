75d637365b15301889e5b7d429f4f96ee7d03606
David Carter <indysoftwaredev@gmail.com>
2023-10-02 09:41:09 -0400

    Injecting configuration for use by the mail service.

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -2,8 +2,14 @@
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@mycompany.com";
-        private string _mailFrom = "noreply@mycompany.com";
+        private readonly string _mailTo = String.Empty;
+        private readonly string _mailFrom = String.Empty;
+
+        public CloudMailService(IConfiguration configuration)
+        {
+            _mailTo = configuration["mailSettings:mailToAddress"];
+            _mailFrom = configuration["mailSettings:mailFromAddress"];
+        }
 
         public void Send(string subject, string message)
         {
