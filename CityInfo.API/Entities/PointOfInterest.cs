0da2c1e73846d7d4001aff0c696a9154a7acb505
David Carter <indysoftwaredev@gmail.com>
2023-10-02 10:08:01 -0400

    Creation of entity classes

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public City? City { get; set; }
+
+        [ForeignKey("CityId")]
+        public int CityId { get; set; }
+
+        public PointOfInterest(string name)
+        {
+            Name = name;
+        }
+    }
+}
