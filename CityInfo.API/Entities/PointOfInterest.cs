bb42277411645625b241cb98db752e7083032951
David Carter <indysoftwaredev@gmail.com>
2023-10-02 11:31:57 -0400

    Adding initial migrations Including additional property on Entity class to show migration updates to the db run update-database to create the db file locally adding *.db to .gitignore

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -11,6 +11,10 @@ namespace CityInfo.API.Entities
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
         public City? City { get; set; }
 
         [ForeignKey("CityId")]
