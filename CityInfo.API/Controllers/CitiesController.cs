395e3498fcdefe8b5f3bc9d3a75b3cb311471915
David Carter <indysoftwaredev@gmail.com>
2023-10-04 10:10:25 -0400

    Adding documentation for return types

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -50,7 +50,11 @@ namespace CityInfo.API.Controllers
         /// <param name="id">The id of the city to get</param>
         /// <param name="includePointsOfInterest">Whether or not to include points of interest</param>
         /// <returns>An IActionResult</returns>
+        /// <response code="200">Returns the requested city.</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetCity(int id, bool includePointsOfInterest = false)
         {
             var city = await _cityInfoRepository.GetCityAsync(id, includePointsOfInterest);
