aad02de8cbd86534ffd2273fce26012779974c75
David Carter <indysoftwaredev@gmail.com>
2023-10-04 09:46:22 -0400

    forgot this file from the previous checkin

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -10,6 +10,7 @@ namespace CityInfo.API.Controllers
     [ApiController]
     //[Authorize]
     [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
     [Route("api/v{version:apiVersion}/cities")]
     public class CitiesController : ControllerBase
     {
